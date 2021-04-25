e096e8c0809ae7c4b67c3abd235513a897ed17a5
Stacey Gammon <gammon@elastic.co>
2021-04-25 11:03:34 -0400

    basic bones

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,87 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { Nav, Navbar, Image, Carousel, Jumbotron, Button } from 'react-bootstrap';
 
 export default function Home() {
+  var background = { maxHeight: '600px' };
+  var textStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+  };
+
+  const CoverPhoto = () => (
+    <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" src="./lake.jpg" alt="First slide" />
+        <Carousel.Caption>
+          <h3>Sidewalks to safely see Loughberry Lake!</h3>
+          <p>We like sidewalks</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://source.unsplash.com/Pw7i-YVg5uM/1600x900"
+          alt="More sidewalks"
+        />
+
+        <Carousel.Caption>
+          <h3>Side walks are so cool</h3>
+          <p>Everyone is just so happy with sidewalks</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Walk Wilton NY</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="#home">Walk Wilton NY</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <Nav.Item>
+                <Nav.Link href="#About">About</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#Contact">Contact</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link href="#Subscribe">Sign the petition</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <div id="About">
+          <CoverPhoto />
+        </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
+        <div id="Contact">
+          <Jumbotron>
+            <h1>Creating a more walkable experience</h1>
+            <p>What should we do to make this possible?</p>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              <Button variant="primary">Learn more</Button>
             </p>
-          </a>
+          </Jumbotron>
+        </div>
+
+        <div id="Subscribe">
+          <Jumbotron>Put some form to sign up to a mailing list? Or sign a petition?</Jumbotron>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        For all those in Wilton, Saratoga County, New York who want to walk around safely.
       </footer>
     </div>
-  )
+  );
 }
