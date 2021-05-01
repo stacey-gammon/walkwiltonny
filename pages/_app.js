d3873f9c8c8a7241230d9b1035eae42d8ba1089a
Stacey Gammon <gammon@elastic.co>
2021-05-01 11:21:54 -0400

    attempt to add goog analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,29 @@
-import '../styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+import * as ga from './ga';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
